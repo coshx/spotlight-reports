20d3489b71a69530c7be8333e0df307e4cf2ae10
Austin Brown <austinmbrown@gmail.com>
2015-06-12 17:28:08 -0400

    now using postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150601190306) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.integer  "canvas_id"
     t.string   "name"
