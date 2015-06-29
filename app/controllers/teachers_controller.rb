6a6dc9af8e70ff94785b188afe3c5ec1cc7f9948
Austin Brown <austinmbrown@gmail.com>
2015-06-29 10:15:55 -0400

    dry refactor

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,20 +1,16 @@
 class TeachersController < ApplicationController
+  before_action :check_login
   require 'httparty'
 
   def index
-    return render html: "<strong>Not Authorized</strong>".html_safe unless session[:school_id]
   end
 
   def get_teachers
-    return render html: "<strong>Not Authorized</strong>".html_safe unless session[:school_id]
-
     render json: Teacher.where(school_account: params[:id]).to_json
   end
 
 
   def get_teacher_details
-    return render html: "<strong>Not Authorized</strong>".html_safe unless session[:school_id]
-
     teacher = Teacher.where(canvas_id: params[:id]).first
     courses = teacher.get_courses
 
@@ -24,6 +20,12 @@ class TeachersController < ApplicationController
     statgrid_data = teacher.compile_statgrid_data(courses)
 
     render json: {teacher: teacher, courses: courses_hash, course_analytics: course_data, statgrid: statgrid_data }
+  end
+
+  private
+
+  def check_login
+    return render html: "<strong>Not Authorized</strong>".html_safe unless session[:school_id]
   end
 
 end
