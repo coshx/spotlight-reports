3c287ad624696e406ae3c4635b3d241c3da3c028
Austin Brown <austinmbrown@gmail.com>
2015-07-29 13:07:04 -0400

    auth bug

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,16 +1,15 @@
 class CoursesController < ApplicationController
 
-  before_action :check_login
+
   skip_before_filter :verify_authenticity_token
 
   def get_school_averages
+    return render html: "<strong>Not Authorized</strong>".html_safe unless session[:school_id]
     render json: Course.average_stats(session[:school_id], params[:start_date], params[:end_date])
   end
 
   private
 
-  def check_login
-    return render html: "<strong>Not Authorized</strong>".html_safe unless session[:school_id]
-  end
+
 
 end
