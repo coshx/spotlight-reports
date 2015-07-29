5f4b405fca7226bd7457b1e0fa437a62d8c9c33c
Austin Brown <austinmbrown@gmail.com>
2015-07-29 14:37:33 -0400

    debugging

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,6 +4,7 @@ class CoursesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def get_school_averages
+    puts 'hello world'
     return render html: "<strong>Not Authorized</strong>".html_safe unless session[:school_id]
     puts session[:school_id], params[:start_date], params[:end_date]
     render json: Course.average_stats(session[:school_id], params[:start_date], params[:end_date])
